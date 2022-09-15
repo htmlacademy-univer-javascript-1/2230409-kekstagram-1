285ab1f3e6cef13f4068f3771f1557e3fe47deb3
b0r1ngx <ghztrikz1337@gmail.com>
2022-09-15 16:59:37 +0300

    add two functions: 1) return a random number N, such that from <= N <= to; 2) return true if string is less than allowed length, else false;

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function randomNumber(from, to) {
+  if (from < 0 || to < 0) {
+    return NaN;
+  } else if (to < from) {
+    return NaN;
+  } else if (from === to) {
+    return from;
+  } else {
+    return from + Math.floor(Math.random() * (to - from + 1));
+  }
+}
+
+function isStringLengthAllowed(string, allowedLength) {
+  return string.length <= allowedLength;
+}
